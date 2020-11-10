235b5cbea79514cd459596707a231ded2f0c4189
unknown <vjeveaux@gmail.com>
2020-11-10 15:38:14 -0300

    Documentação API com Swagger

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using Shop.Data;
 using Shop.Models;
 using Microsoft.AspNetCore.Authorization;
-using System.Linq;
 using Shop.Services;
 
 [Route("products")]
--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,17 @@
+  
+using System.Linq;
 using System.Text;
+using Shop.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Shop.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 
 namespace Shop
@@ -23,6 +27,12 @@ namespace Shop
 
          public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/json" });
+            });
             services.AddControllers();
             var key = Encoding.ASCII.GetBytes(Settings.Secret);
             services.AddAuthentication(x =>
@@ -48,6 +58,10 @@ namespace Shop
             //services.AddDbContext<DataContext>(opt=> opt.UseInMemoryDatabase("Database"));
             services.AddDbContext<DataContext>(opt=> opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
             services.AddScoped<DataContext, DataContext>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shop Api", Version = "v1" });
+            });
         }
 
 
@@ -59,12 +73,15 @@ namespace Shop
             }
 
             app.UseHttpsRedirection();
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shop API V1");
+            });
             app.UseRouting();
-
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
